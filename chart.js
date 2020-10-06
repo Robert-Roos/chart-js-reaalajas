e8c083581ad224e9308b1a6151c08e987c5d9643
Robert-Roos <64014141+Robert-Roos@users.noreply.github.com>
2020-10-07 00:36:18 +0300

    graaf loeb andmeid eleringist
    vaja manipuleerida for tsükli väljatrükki, et nad graafikule saaks
    x-telg on aeg, y-telg on hind

--- chart.js
+++ chart.js
@@ -1,21 +1,58 @@
-var ctx = document.getElementById('myChart');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+const xtelg = [];
+const ytelg = [];
 
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
+async function graaf() {
+    await getData();
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: xtelg,
+            datasets: [{
+                label: 'Gaasi hind',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: ytelg,
+            }]
+        },
+        options: {
+            responsive: false
+        }
+    });
+}
+graaf();
 
-    // Configuration options go here
-    options: {
-        responsive: false
-    }
-});
+async function getData() {
+    const url = 'https://dashboard.elering.ee/api/gas-balance/price?end=2020-11-20T11%3A11%3A11Z&start=2020-09-20T11%3A11%3A11Z'
+    const response = await fetch(url);
+    const data = await response.json();
+    const kuuPaev = new Date(data.data[0].timestamp*1000);
+    var koikKuuPaevad, i, x ="";
+        for(i in data.data){
+            x += data.data[i].timestamp;
+        }
+        console.log(x)
+    xtelg.push(kuuPaev.toLocaleString());
+
+    const ostuHind = data.data[0].imbalance_buy_price;
+    var koikHinnad, i, x = ""; 
+        for(i in data.data){
+            x += data.data[i].imbalance_buy_price;
+        }
+        console.log(x)
+    ytelg.push(ostuHind);
+
+
+    //const myygiHind = data.data[0].imbalance_sell_price;
+    console.log(kuuPaev.toLocaleString());
+    console.log(ostuHind)
+    //console.log(myygiHind) 
+    //console.log(data.data)
+}
+
+
+
+
+
+//elering api tahab sisestatud aega kujul 2020-11-20T11:11:11Z
+//epoch
